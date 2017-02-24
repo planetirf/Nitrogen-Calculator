623662cecf55ec04da5fdc91aa906ebdf7c3378c
Irfan Ainuddin <irfandin@gmail.com>
2017-02-23 16:25:59 -0800

    HAHA OMG IT WORKS! DATA FROM NESTED JSON, DYNAMIC HTML, SEPERATED FILES!
    Input Section halfway complete.

--- scripts.js
+++ scripts.js
@@ -1,25 +1,74 @@
+// Irfan Ainuddin
+// Get data from data.json populated crop selector and load <div id='input'> innerHTML.
 
-// Load Data Upon Crop Selection
-function loadData() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
+var cropData = data;
+var inputHTML = '<div>' + 'Field ID: ' + '<input type="text" id="fieldId"><br><br>';
+var cropSelector = 'Select a crop: ' + '<select id="cropSelector">';
+var yieldInput = 'Expected Yield: ' + '<input type="text" id="expectedYield">';
+var unitsInput = ' Units:' + '<select id="units">';
+var residueRemovedBox = 'Check box if residue is removed' + '<input type= "checkbox" id="residueRemoved" /><br><br>';
+var percentRemovedInput = 'Percent of plant residue removed: ' + '<input type="text" id="percentRemovedInput"><br><br>';
+// Planting and Harvest Date inputs to be added
+
+function getData() {
+
+  // fetch crop list
+  for (var i in cropData) {
+    cropName = cropData[i]['crops'][i]['name'];
+    cropChoices = cropData[i]['crops'];
+    //console.log(cropChoices);
+
+  // add each crop in cropChoices to cropSelector dropdown menu
+    for (var j in cropChoices){
+      //console.log(cropChoices[j]['name']);
+      name = cropChoices[j]['name'];
+       cropSelector += "<option value=" + name + ">" + name + "</option>";
     }
-  };
-  xhttp.open("GET", "cropData.csv", true);
-  xhttp.send();
-  alert(xhttp);
+
+    // close the cropSelector options and add into inputHTML
+    cropSelector += '</select><br><br>';
+    inputHTML += cropSelector;
+
+  }
+  // Build Input Skeleton - will need to refactor to get "moving parts" working
+  // Yield Input
+
+  unitsInput += '</select><br><br>';
+  yieldInput += unitsInput;
+  inputHTML += yieldInput;
+  inputHTML += residueRemovedBox;
+  inputHTML += percentRemovedInput;
+
+
+// close inputHTML
+  inputHTML += ' OHAY ' + '</div>';
+  document.getElementById('input').innerHTML = inputHTML;
+};
+// Call getData function to populate input div.
+getData();
+
+
+
+//document.getElementById('cropSelector').addEventListener("load", unitSelector);
+document.getElementById('cropSelector').addEventListener("change", unitSelector);
+
+function unitSelector() {
+    var cropData = data;
+    var cropChoices = cropData[0]['crops']
+    var currentCrop = document.getElementById('cropSelector').value;
+    //console.log(currentCrop);
+     for (i = 0; i < cropChoices.length; i++) {
+       //console.log(cropChoices[i]['name']);
+       if (currentCrop === cropChoices[i]['name']) {
+         console.log(currentCrop);
+
+       }
+     }
 
 }
 
-///////////////// Read Data from CSV file TEST //////////////////////////
-///// *** Javascript difficulties with reading an local file durin
-////////////////////////////////////////////////////////////////////////
-document.getElementById('cropSelect').addEventListener("change", myFunction);
-
-/*  sample array to test calculators */
+// Call unitSelector function to initialize units
+unitSelector();
 
 function myFunction() {
     var mydata = data;
@@ -31,15 +80,15 @@ function myFunction() {
 }
 
 ////////// Variables Targeting Input and Output Values ////////////////
-var crop = document.getElementById("cropSelect").value;
-//var expectedYield = document.getElementById("ExpectedYield").value;
-var units = document.getElementById("Units").value;
-var strawRemoved = document.getElementById("StrawRemoved").value;
-//var percentRemoved = document.getElementById("PercentRemoved").value;
-var plantingDate = document.getElementById("PlantingDate").value;
-var plantingMethod = document.getElementById("PlantingMethod").value;
-var harvestDate = document.getElementById("HarvestDate").value;
-
+var crop = document.getElementById("cropSelector").value;
+var expectedYield = document.getElementById("expectedYield").value;
+var units = document.getElementById("units").value;
+var residueRemoved = document.getElementById("residueRemovedBox");
+var percentRemoved = document.getElementById("percentRemovedInput").value;
+// var plantingDate = document.getElementById("PlantingDate").value;
+// var plantingMethod = document.getElementById("PlantingMethod").value;
+// var harvestDate = document.getElementById("HarvestDate").value;
+//
 function Calculate() {
   var Nconc = document.getElementById('NConc').innerHTML = cropJSON.percentN;
   var expectedYield = document.getElementById("ExpectedYield").value;
